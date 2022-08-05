f85fed4c46fc125f32c0b7856440edc3271d6e12
Gustav Grusell <gustav.grusell@svt.se>
2022-08-05 11:33:15 +0200

    Update formula to pass audit
    Signed-off-by: Gustav Grusell <gustav.grusell@svt.se>

--- Formula/svt-av1.rb
+++ Formula/svt-av1.rb
@@ -1,18 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class SvtAv1 < Formula
-  desc "SVT-AV1 video codec"
+  desc "AV1 encoder"
   homepage "https://gitlab.com/AOMediaCodec/SVT-AV1"
   url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.1.0/SVT-AV1-v1.1.0.tar.gz"
   sha256 "1c211b944ac83ef013fe91aee96c01289da4e3762c1e2f265449f3a964f8e4bc"
-  head "https://gitlab.com/AOMediaCodec/SVT-AV1"
   license "BSD-3-Clause"
+  head "https://gitlab.com/AOMediaCodec/SVT-AV1.git", branch: "master"
 
   depends_on "cmake" => :build
   depends_on "make" => :build
   depends_on "yasm" => :build
-  
+
+  resource("testvideo") do
+    url "https://github.com/grusell/svt-av1-homebrew-testdata/raw/main/video_64x64_yuv420p_25frames.yuv"
+    sha256 "0c5cc90b079d0d9c1ded1376357d23a9782a704a83e01731f50ccd162e246492"
+  end
+
   def install
     Dir.chdir("Build")
     system "cmake", "..", "-G", "Unix Makefiles", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
@@ -21,6 +23,8 @@ class SvtAv1 < Formula
   end
 
   test do
-    system "#{bin}/SvtAv1EncApp", "--help"
+    testpath.install resource("testvideo")
+    system "#{bin}/SvtAv1EncApp", "-w", "64", "-h", "64", "-i", "video_64x64_yuv420p_25frames.yuv", "-b", "output.ivf"
+    assert_predicate testpath/"output.ivf", :exist?
   end
 end
